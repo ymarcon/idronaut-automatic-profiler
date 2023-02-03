5c0931f2f89a7b6a19c73824a5a8c0d297894785
Camille Minaudo <camille.minaudo@epfl.ch>
2023-02-03 17:27:40 +0000

    Fix bugs

--- scripts/input_batch.bat
+++ scripts/input_batch.bat
@@ -3,8 +3,9 @@
 :::: DO NOT CHANGE THE VARIABLE NAMES ::::
 
 :: Define python environment
-SET pythonenv="C:\Users\Seatronic 1147\Documents\Data_Lexplore\git\venv\Scripts\python"
+SET pythonenv="C:\Users\Seatronic 1147\Documents\Data_Lexplore\git\venv39\Scripts\python"
 
 :: Python script
 SET script="C:\Users\Seatronic 1147\Documents\Data_Lexplore\git\idronaut-automatic-profiler\scripts\main.py"
+SET upload="C:\Users\Seatronic 1147\Documents\Data_Lexplore\git\idronaut-automatic-profiler\scripts\upload_remote_data.py"
 
